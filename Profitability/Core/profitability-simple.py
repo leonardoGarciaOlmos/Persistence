ccc9aada46c9831a9dd48287d92083384aad8160
Leonardo Garcia <leonardogarciaolmos.12@gmail.com>
2020-09-17 20:00:09 -0300

    Refactorizacion a la clase rentabilidad simple
    * Se valida que los valores de entradas sean solo tipo flotante
    * se valida que los valores de entradas no sean ambos 0
    * Se limita el numero de decimales en el retorno de la función

--- Profitability/Core/profitability-simple.py
+++
@@ -1,6 +0,0 @@
-class ProfitabilitySimple():
-
-    def Calculate_Profitability_Simple(self, valueEnd, valueBegin):
-        retProfitability = ((valueEnd - valueBegin) / valueBegin) * 100
-        return retProfitability
-        
